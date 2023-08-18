78dc21d3fdf98e812b95364ddb225809fa7bf835
benpaodewoniu <1506316219@qq.com>
2023-08-18 23:00:52 +0800

    'update'

--- upload.bat
+++ upload.bat
@@ -1,4 +1,11 @@
-rd /s /Q D:\TransactionRecord\web
+rd /s /Q D:\TransactionRecord\_book
+git switch master
+git add .
 git commit -m 'update'
-git push origin main
-md D:\TransactionRecord\web
+git push origin master
+gitbook build
+cd _book
+git switch gh-pages
+git add .
+git commit -m 'update'
+git push origin gh-pages
